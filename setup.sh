3d086382e72f770596dbaf3520595394b34acd2e
affanhamid <affanhamid007@gmail.com>
2024-06-20 00:35:08 +0530

    test setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,8 @@ source trading_env/bin/activate
 cd ~
 # wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 # tar -xzvf ta-lib-0.4.0-src.tar.gz
-# cd ta-lib
+mkdir ta-lib
+cd ta-lib
 # ./configure --prefix=/usr
 # make
 # sudo make install
